794bf8b10e69d21ce560b5301623f623d1d33876
chahatbegmal20 <chahatbegmal@gmail.com>
2023-11-18 01:39:14 +0530

    minor changes

--- src/app/createAccount/page.js
+++ src/app/createAccount/page.js
@@ -1,6 +1,9 @@
+"use client";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const CreateAccount = () => {
+  const router = useRouter();
   return (
     <div id="app">
       <section className="bg-gray-50">
@@ -10,7 +13,13 @@ const CreateAccount = () => {
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Create your account
               </h1>
-              <form className="space-y-4 md:space-y-6">
+              <form
+                className="space-y-4 md:space-y-6"
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  router.push("/userTable");
+                }}
+              >
                 <div>
                   <label
                     htmlFor="email"
@@ -48,7 +57,7 @@ const CreateAccount = () => {
                   type="submit"
                   class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                 >
-                  Sign in
+                  Create
                 </button>
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                   Already have an account?{" "}
